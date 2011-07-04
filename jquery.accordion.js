e169fd7fee82171885c67ed5482cc443f41e2b89
Sam Croft <samcroft@gmail.com>
2011-07-04 11:05:58 +0100

    Added url hash support to open a specific accordion element

--- jquery.accordion.js
+++ jquery.accordion.js
@@ -5,14 +5,27 @@
 		ddHeight = new Array();
 		
 		el.addClass('enhance');
-
-		el.find('dd').each(function(i){
+		
+		el.find('dd').each(function(i) {
 			var dd = $(this);
 			ddHeight[i] = dd.height();
-			dd.addClass('closed')
+			dd.addClass('closed');
 		});
 		
-		el.find('dt a').bind('touchstart', function(e) {
+		var hash = location.hash;
+		var hasHash = el.find('dt a[href="'+hash+'"]');
+		
+		if (hasHash.length) {
+			var toExpand = hasHash.parent().next('dd');
+			var i = toExpand.index('dd');
+			
+			toExpand
+				.attr('id', 'active')
+				.css('height', ddHeight[i]+'px')
+				.removeClass('closed');
+		}
+		
+		el.find('dt a').bind('click', function(e) {
 			e.preventDefault();
 			
 			var toExpand = $(this).parent().next('dd');
@@ -23,6 +36,8 @@
 					.removeAttr('id')
 					.removeAttr('style')
 					.addClass('closed');
+					
+				location.hash = '';
 			} else {
 				var active = toExpand.parent().find('#active');
 
@@ -37,6 +52,8 @@
 					.attr('id', 'active')
 					.css('height', ddHeight[i]+'px')
 					.removeClass('closed');
+					
+				location.hash = $(this).attr('href');
 			}
 		});
 	}
